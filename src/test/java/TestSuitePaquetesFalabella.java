f9ad16a96e40c41d7c290d5ff0cc0282a9f87c3b
Israel Garate <israelgarate@gmail.com>
2022-04-29 00:52:00 -0400

    se sube ptc05

--- src/test/java/TestSuitePaquetesFalabella.java
+++ src/test/java/TestSuitePaquetesFalabella.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -43,6 +44,27 @@ public class TestSuitePaquetesFalabella {
     public By localizadorAgregarAutoCompacto = By.xpath("//a[@class=\"eva-3-btn-ghost -lg -primary\"]");
     public By localizadorNextCompra = By.xpath("//button[@class=\"eva-3-btn -lg pricebox-sticky-button -secondary\"]");
     public By localizadorApurate = By.xpath("(//span [@class=\"eva-3-h5 message-title\"])[1]");
+    public By localizadorPaquetesCaribe = By.xpath("(//optional-link)[2]");
+    public By localizadorPaquetesCancun = By.xpath("(//img[@class=\"offer-card-image-main\"])[4]");
+    public By localizadorPaqueteACancun = By.xpath("(//div[@class=\"date-container__price col -sm-12\"])[1]");
+    public By localizadorVerDetalle = By.xpath("(//a[@class=\"eva-3-btn -md -primary \"])[2]");
+    public By localizadorCambiarALojamiento = By.xpath("(//a[@class=\"eva-3-link ng-star-inserted\"])[2]");
+    public By localizadorDetalleAlojamiento = By.xpath("(//a[@class=\"eva-3-btn-ghost -md -eva-3-fwidth\"])[3]");
+    public By localizadorVerMapa = By.xpath("//button[@class=\"eva-3-btn -lg -white -eva-3-shadow-static\"]");
+    public By localizadorVista360 = By.xpath("(//i[@class=\"btn-icon eva-3-icon-view-360\"])");
+    public By localizadorCerrarVista = By.xpath("//i[@class=\"eva-3-icon-close modal-fullscreen-close\"]");
+    public By localizadorSuiteJunior = By.xpath("(//i[@class=\"radio-circle\"])[3]");
+    public By localizadorAceptarSuite = By.xpath("(//button[@class=\"eva-3-btn -md -primary -eva-3-fwidth\"])[2]");
+    public By localizadorVerGaleria = By.xpath("(//a[@class=\"eva-3-btn -md -white mosaic-gallery btn-gallery ng-star-inserted\"])");
+    public By localizadorNextImage = By.xpath("(//i[@class=\"eva-3-icon-arrow-down gallery-arrow arrow-right\"])");
+    public By localizadorCerrarGaleria = By.xpath("(//i[@class=\"eva-3-icon-close gallery-close\"])");
+    public By localizadorVuelo = By.xpath("(//em[@class=\"btn-text ng-star-inserted\"])[1]");
+    public By localizadorEquipajeBodega2 = By.xpath("(//i[@class=\"checkbox-check eva-3-icon-checkmark filters-checkbox-left\"])[6]");
+    public By localizadorNuevoVuelo = By.xpath("(//a[@class=\"-md eva-3-btn-ghost\"])[1]");
+    public By localizadorSiguienteComprar = By.xpath("(//button[@class=\"eva-3-btn -lg pricebox-sticky-button -secondary\"])");
+    public By localizadorNextPaquete =By.xpath("//i[@class=\"nav-slider-icon eva-3-icon-arrow-right\"]");
+
+
     @BeforeClass
     public static void init(){
         WebDriverManager.chromedriver().setup();
@@ -142,7 +164,167 @@ public class TestSuitePaquetesFalabella {
         btnBuscar.click();
     }
 
-    @Test public void PTC06()  {
+    @Test public void PTC05() {
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofMillis(1000))
+                .withMessage("Error de timeout BC(")
+                .ignoring(NoSuchElementException.class);
+
+        //Abrir la pagina
+        driver.get("https://www.viajesfalabella.cl/paquetes");
+        WebElement cookie = driver.findElement(localizadorBtnCookie);
+        if(cookie.isDisplayed()){
+            cookie.click();
+        }
+
+        try {
+            WebElement caribe = driver.findElement(localizadorPaquetesCaribe);
+            wait.until(ExpectedConditions.elementToBeClickable(caribe));
+            caribe.click();
+            WebElement puntaCana = driver.findElement(localizadorPaquetesCancun);
+            wait.until(ExpectedConditions.elementToBeClickable(puntaCana));
+            puntaCana.click();
+            WebElement btnBuscarPaquete = driver.findElement(localizadorPaqueteACancun);
+            wait.until(ExpectedConditions.elementToBeClickable(btnBuscarPaquete));
+            btnBuscarPaquete.click();
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement caribe = driver.findElement(localizadorPaquetesCaribe);
+            wait.until(ExpectedConditions.elementToBeClickable(caribe));
+            caribe.click();
+            WebElement cancun = driver.findElement(localizadorPaquetesCancun);
+            wait.until(ExpectedConditions.elementToBeClickable(cancun));
+            cancun.click();
+            WebElement btnBuscarPaquete = driver.findElement(localizadorPaqueteACancun);
+            wait.until(ExpectedConditions.elementToBeClickable(btnBuscarPaquete));
+            btnBuscarPaquete.click();
+        }
+        // ir a otra ventana
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        driver.switchTo().window(driver.getWindowHandles().toArray()[1].toString());
+        // ir a otra ventana
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        /*WebElement nextPage = driver.findElement(localizadorNextPaquete);
+        nextPage.click();*/
+        try {
+            WebElement verDetalle = driver.findElement(localizadorVerDetalle);
+            wait.until(ExpectedConditions.elementToBeClickable(verDetalle));
+            jse.executeScript("arguments[0].click();", verDetalle);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement verDetalle = driver.findElement(localizadorVerDetalle);
+            wait.until(ExpectedConditions.elementToBeClickable(verDetalle));
+            verDetalle.click();
+        }
+        // ir a otra ventana
+        wait.until(ExpectedConditions.numberOfWindowsToBe(3));
+        driver.switchTo().window(driver.getWindowHandles().toArray()[2].toString());
+        // ir a otra ventana
+        try {
+            WebElement alojamiento = driver.findElement(localizadorCambiarALojamiento);
+            wait.until(ExpectedConditions.elementToBeClickable(alojamiento));
+            jse.executeScript("arguments[0].click();", alojamiento);
+            WebElement verDetalleAlojamiento = driver.findElement(localizadorDetalleAlojamiento);
+            wait.until(ExpectedConditions.elementToBeClickable(verDetalleAlojamiento));
+            jse.executeScript("arguments[0].click();", verDetalleAlojamiento);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement alojamiento = driver.findElement(localizadorCambiarALojamiento);
+            wait.until(ExpectedConditions.elementToBeClickable(alojamiento));
+            jse.executeScript("arguments[0].click();", alojamiento);
+            WebElement verDetalleAlojamiento = driver.findElement(localizadorDetalleAlojamiento);
+            wait.until(ExpectedConditions.elementToBeClickable(verDetalleAlojamiento));
+            jse.executeScript("arguments[0].click();", verDetalleAlojamiento);
+        }
+
+        try {
+            WebElement verMapa = driver.findElement(localizadorVerMapa);
+            wait.until(ExpectedConditions.elementToBeClickable(verMapa));
+            jse.executeScript("arguments[0].click();", verMapa);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement verMapa = driver.findElement(localizadorVerMapa);
+            wait.until(ExpectedConditions.elementToBeClickable(verMapa));
+            verMapa.click();
+        }
+        try {
+            WebElement vista360 = driver.findElement(localizadorVista360);
+            wait.until(ExpectedConditions.elementToBeClickable(vista360));
+            jse.executeScript("arguments[0].click();", vista360);
+            WebElement btnCerrar360 = driver.findElement(localizadorCerrarVista);
+            wait.until(ExpectedConditions.elementToBeClickable(btnCerrar360));
+            jse.executeScript("arguments[0].click();", btnCerrar360);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement vista360 = driver.findElement(localizadorVista360);
+            wait.until(ExpectedConditions.elementToBeClickable(vista360));
+            jse.executeScript("arguments[0].click();", vista360);
+            WebElement btnCerrar360 = driver.findElement(localizadorCerrarVista);
+            wait.until(ExpectedConditions.elementToBeClickable(btnCerrar360));
+            jse.executeScript("arguments[0].click();", btnCerrar360);
+        }
+        WebElement suite = driver.findElement(localizadorSuiteJunior);
+        wait.until(ExpectedConditions.elementToBeClickable(suite));
+        jse.executeScript("arguments[0].click();", suite);
+        WebElement aceptarSuite = driver.findElement(localizadorAceptarSuite);
+        wait.until(ExpectedConditions.elementToBeClickable(aceptarSuite));
+        jse.executeScript("arguments[0].click();", aceptarSuite);
+        WebElement verGaleria = driver.findElement(localizadorVerGaleria);
+        wait.until(ExpectedConditions.elementToBeClickable(verGaleria));
+        jse.executeScript("arguments[0].click();", verGaleria);
+        WebElement btnVerGaleria = driver.findElement(localizadorVerGaleria);
+        jse.executeScript("arguments[0].click();", btnVerGaleria);
+        wait.until(ExpectedConditions.elementToBeClickable(btnVerGaleria));
+        for (int i = 0; i < 7; i++) {
+            WebElement galeriaNext = driver.findElement(localizadorNextImage);
+            wait.until(ExpectedConditions.elementToBeClickable(galeriaNext));
+            galeriaNext.click();
+        }
+        try {
+            WebElement btnCerrarGaleria = driver.findElement(localizadorCerrarGaleria);
+            wait.until(ExpectedConditions.elementToBeClickable(btnCerrarGaleria));
+            jse.executeScript("arguments[0].click();", btnCerrarGaleria);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement btnCerrarGaleria = driver.findElement(localizadorCerrarGaleria);
+            wait.until(ExpectedConditions.elementToBeClickable(btnCerrarGaleria));
+            jse.executeScript("arguments[0].click();", btnCerrarGaleria);
+        }
+        WebElement vuelo = driver.findElement(localizadorVuelo);
+        wait.until(ExpectedConditions.elementToBeClickable(vuelo));
+        jse.executeScript("arguments[0].click();", vuelo);
+        WebElement equipajeBodega = driver.findElement(localizadorEquipajeBodega2);
+        wait.until(ExpectedConditions.elementToBeClickable(equipajeBodega));
+        equipajeBodega.click();
+        try {
+            WebElement nuevoVuelo = driver.findElement(localizadorNuevoVuelo);
+            wait.until(ExpectedConditions.elementToBeClickable(nuevoVuelo));
+            jse.executeScript("arguments[0].click();", nuevoVuelo);
+        }  catch (org.openqa.selenium.StaleElementReferenceException ex) {
+            WebElement nuevoVuelo = driver.findElement(localizadorNuevoVuelo);
+            wait.until(ExpectedConditions.elementToBeClickable(nuevoVuelo));
+            jse.executeScript("arguments[0].click();", nuevoVuelo);
+        }
+        WebElement verElemento = driver.findElement(By.xpath("(//em[@class=\"btn-text ng-star-inserted\"])[5]"));
+        wait.until(ExpectedConditions.elementToBeClickable(verElemento));
+        jse.executeScript("arguments[0].click();", verElemento);
+        WebElement cerrarElemento = driver.findElement(By.xpath("(//i[@class=\"modal-close eva-3-icon-close\"])[2]"));
+        wait.until(ExpectedConditions.elementToBeClickable(cerrarElemento));
+        cerrarElemento.click();
+        try {
+            WebElement comprar = driver.findElement(localizadorSiguienteComprar);
+            wait.until(ExpectedConditions.elementToBeClickable(comprar));
+            comprar.click();
+        }  catch (org.openqa.selenium.InvalidSelectorException ex) {
+            WebElement comprar = driver.findElement(localizadorSiguienteComprar);
+            wait.until(ExpectedConditions.elementToBeClickable(comprar));
+            comprar.click();
+        }
+        WebElement apurate = driver.findElement(localizadorApurate);
+        wait.until(ExpectedConditions.elementToBeClickable(apurate));
+        Assert.assertEquals(apurate.getText(), "¡Apúrate!");
+
+    }
+
+
+
+    @Test public void PTC06(){
         int mesViajeIda = 6;
         int mesViajeVuelta = 6;
         int diaViajeIda = 1;
@@ -304,7 +486,6 @@ public class TestSuitePaquetesFalabella {
             jse.executeScript("arguments[0].click();", btnAgregarAutoCom);
             WebElement btnNextComprar = driver.findElement(localizadorNextCompra);
             jse.executeScript("arguments[0].click();", btnNextComprar);
-
         }
         catch(org.openqa.selenium.ElementClickInterceptedException ex)
         {
